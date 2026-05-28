5912f286293d2c612028eef81b2c55cf58b0b707
Eduardo Orsi <eduardo.orsi@phddobrasil.com>
2026-05-28 18:31:44 -0300

    WIP: Ajusta store domain

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,13 +1,22 @@
 import { defineMiddleware } from "nitro";
+import { env } from "~/server/utils/env.ts";
 
 const SHOPIFY_ORIGIN = /^https:\/\/(?:[a-z0-9-]+\.)?(?:shopify|myshopify)\.com$/;
+
+function isAllowedOrigin(origin: string): boolean {
+  if (SHOPIFY_ORIGIN.test(origin))
+    return true;
+  if (env.shopifyStoreDomain && origin === `https://${env.shopifyStoreDomain}`)
+    return true;
+  return false;
+}
 
 export default defineMiddleware((event) => {
   if (event.req.url.includes("/api/webhooks/"))
     return;
 
   const origin = event.req.headers.get("origin");
-  if (!origin || !SHOPIFY_ORIGIN.test(origin))
+  if (!origin || !isAllowedOrigin(origin))
     return;
 
   event.res.headers.set("Access-Control-Allow-Origin", origin);
