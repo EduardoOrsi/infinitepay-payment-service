6915b7224c883283a9d42316ceda948f8288f5ee
Eduardo Orsi <eduardo.orsifx@gmail.com>
2026-05-28 18:35:58 -0300

    FIX: CORS

--- nitro.config.ts
+++ nitro.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   preset: "vercel",
   serverDir: "./server",
   routeRules: {
+    "/api/checkout/**": {
+      cors: true,
+    },
     "/api/webhooks/**": {
       headers: {
         "access-control-allow-methods": "POST",
